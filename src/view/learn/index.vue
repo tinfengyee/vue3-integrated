b0d76547531fb36bbbd04037cf41f65082bc006d
tinfengyee <tinfengyee@163.com>
2022-09-20 23:36:37 +0800

    update a bit

---
+++ src/view/learn/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { reactive } from 'vue'
+
+  export default {
+    setup() {
+      const state = reactive({
+        count: 0,
+      })
+      return {
+        state,
+      }
+    },
+  }
+</script>
+
+<template>
+  <div>Learn {{ state.count }}</div>
+</template>
